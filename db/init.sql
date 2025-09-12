bfa3e77c0be22b57e164d7d8c58d88ac66fe6728
Carey Gatz <carey@teamstransport.com>
2025-09-12 12:59:12 -0500

    updated init file to properly import case models

--- db/init.sql
+++ db/init.sql
@@ -337,7 +337,7 @@ INTO TABLE stg_quote
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(ID, QuoteNum, @var_quote_date, @var_quote_expiry, SAPVendor, PurchaseOrder,
+(ID, QuoteNum, @var_quote_date, @var_quote_expiry, SAPVendor, @var_purchase_order,
  CompStoreID, PreparedBy, SalesRep, StoreJobPM)
 SET QuoteDate = STR_TO_DATE(@var_quote_date, '%m/%d/%Y %H:%i'),
     QuoteExpiry = STR_TO_DATE(@var_quote_expiry, '%m/%d/%Y %H:%i');
